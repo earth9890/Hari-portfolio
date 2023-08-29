43ba8fa2078f48f2707a0c636d4b22b6452366a9
Harish Dhanraj Sugandhi <68777106+harishsganndhi123@users.noreply.github.com>
2023-08-30 01:32:18 +0530

    First Commit

---
+++ src/content_option.js
@@ -0,0 +1,130 @@
+const logotext = "Hari";
+const meta = {
+  title: "Harish Sugandhi",
+  description:
+    "I’m Harish Sugandhi Software Engineer, Full stack devloper,currently persuing Bachelors of Computer Engineering",
+};
+
+const introdata = {
+  title: "I’m Harish D. Sugandhi",
+  animated: {
+    first: "I love coding",
+    second: "I love competitive programming",
+    third: "I develop websites and mobile apps",
+    
+  },
+  description: "",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: " About my self",
+  aboutme:
+    "Hello, I am Harish and I am a software developer and I love competitive programming. I am from Solapur but currently lives in Pune and I am in last year of my Bachelors in Computer Engineering. I develop mobile apps and website. I am more familiar to work with C++, Java and MERN stack. ",
+};
+const worktimeline = [
+  {
+    jobtitle: "",
+    where: "",
+    date: "",
+  },
+ 
+];
+
+const skills = [
+  {
+    name: "C++",
+    value: 90,
+  },
+  {
+    name: "Java",
+    value: 85,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "React",
+    value: 60,
+  },
+  {
+    name: "MERN",
+    value: 85,
+    },
+    {
+        name: "MySQL",
+        value : 75,
+    },
+    {
+        name: "Android",
+        value : 78,
+    }
+  
+];
+
+const services = [
+  {
+    title: "Web Development",
+    description:
+      "",
+  },
+  {
+    title: "Mobile Apps",
+    description:
+      "",
+  },
+ 
+];
+
+const dataportfolio = [
+  {
+    img: "https://plus.unsplash.com/premium_photo-1661297461253-ae1968b5d46c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80",
+    description:
+      "Smart Home Project",
+    link: "https://github.com/harishsganndhi123/Android-App-for-Home-Automation-to-Control-Bluetooth--HC-05",
+  },
+
+  {
+    img: "https://images.unsplash.com/photo-1529390079861-591de354faf5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHN0dWRlbnQlMjBleGFtJTIwc3lzdGVtfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+    description:
+      "Student Examination System",
+    link: "https://github.com/harishsganndhi123/Java-Based-Examination-Portal-Using-Mysql-and-JSwing-Basics-",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1555529733-0e670560f7e1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2787&q=80",
+    description:
+      "T-Shirt Selling E-Commerce Website",
+    link: "https://github.com/harishsganndhi123/E-Commerce-Website",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "harishsugandhi.work@gmail.com",
+  YOUR_FONE: "(+91) 9890-24-7437",
+  description: "",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_zelsp2e",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/harishsganndhi123/",
+  facebook: "https://www.facebook.com/harish.sugandhi.14",
+  linkedin: "https://www.linkedin.com/in/harish-sugandhi-709bba225/",
+  twitter: "https://twitter.com/harish_sug87023",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
